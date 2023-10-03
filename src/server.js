be86d8049e7237e45865415ef6b00b0d7016f385
Rafael Uribe <53197581+Rafael3226@users.noreply.github.com>
2023-10-02 23:34:37 -0500

    Linter changes

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
-const app = require('./app');
+const process = require("node:process");
+const app = require("./app");
 
 init();
 
 async function init() {
   try {
     app.listen(3001, () => {
-      console.log('Express App Listening on Port 3001');
+      console.log("Express App Listening on Port 3001");
     });
   } catch (error) {
     console.error(`An error occurred: ${JSON.stringify(error)}`);
