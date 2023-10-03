ac0086712ae775d9de354c2089b64437d2074044
Rafael Uribe <53197581+Rafael3226@users.noreply.github.com>
2023-10-03 12:21:18 -0500

    Testing for get unpaid jobs endpoint

---
+++ src/routes/test/job.test.js
@@ -0,0 +1,24 @@
+const { describe, it, expect } = require("@jest/globals");
+const request = require("supertest");
+const app = require("../../app");
+
+describe("Get unpaid jobs", () => {
+  it("should find unpaid jobs with prifile 1", async () => {
+    const response = await request(app)
+      .get("/jobs/unpaid")
+      .set("profile_id", "1");
+    expect(response.statusCode).toBe(200);
+  });
+  it("should find unpaid jobs with prifile 2", async () => {
+    const response = await request(app)
+      .get("/jobs/unpaid")
+      .set("profile_id", "2");
+    expect(response.statusCode).toBe(200);
+  });
+  it("should NOT find unpaid jobs with prifile 3", async () => {
+    const response = await request(app)
+      .get("/jobs/unpaid")
+      .set("profile_id", "3");
+    expect(response.statusCode).toBe(404);
+  });
+});
