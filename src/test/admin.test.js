b407fa300d0574faf1d3380d5b620a3009e1b47e
Rafael Uribe <53197581+Rafael3226@users.noreply.github.com>
2023-10-04 18:28:52 -0500

    test best client enpoint

--- src/test/admin.test.js
+++ src/test/admin.test.js
@@ -3,12 +3,14 @@ const request = require("supertest");
 const app = require("../app");
 const { errorMessages } = require("../services/adminService");
 
+const start = "2019-08-15T19:11:26.737Z";
+const end = "2021-08-15T19:11:26.737Z";
+
 describe("Get best profession", () => {
-  const start = "2019-08-15T19:11:26.737Z";
-  const end = "2021-08-15T19:11:26.737Z";
+  const uri = "/admin/best-profession";
   it("should find Programmer as best profession", async () => {
     const response = await request(app)
-      .get("/admin/best-profession")
+      .get(uri)
       .set("profile_id", "1")
       .query({ start, end });
     expect(response.statusCode).toBe(200);
@@ -17,7 +19,46 @@ describe("Get best profession", () => {
   });
   it("should find Programmer as best profession", async () => {
     const response = await request(app)
-      .get("/admin/best-profession")
+      .get(uri)
+      .set("profile_id", "1")
+      .query({ start: end, end: start });
+    expect(response.statusCode).toBe(400);
+    expect(response.body.error.message).toBe(errorMessages.dateError);
+  });
+});
+
+describe("Get best clients", () => {
+  const uri = "/admin/best-clients";
+  it("should find 2 (default) best clients", async () => {
+    const response = await request(app)
+      .get(uri)
+      .set("profile_id", "1")
+      .query({ start, end });
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBe(2);
+  });
+  it("should be sorted DESC", async () => {
+    const response = await request(app)
+      .get(uri)
+      .set("profile_id", "1")
+      .query({ start, end });
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveLength(2);
+    expect(response.body[0].totalSpends).toBeGreaterThan(
+      response.body[1].totalSpends
+    );
+  });
+  it("should find 3 best clients", async () => {
+    const response = await request(app)
+      .get(uri)
+      .set("profile_id", "1")
+      .query({ start, end, limit: 3 });
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveLength(3);
+  });
+  it("should throw a date error", async () => {
+    const response = await request(app)
+      .get(uri)
       .set("profile_id", "1")
       .query({ start: end, end: start });
     expect(response.statusCode).toBe(400);
